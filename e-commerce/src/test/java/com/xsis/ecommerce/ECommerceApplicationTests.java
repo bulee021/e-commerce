cb9e5ed8783a0937afa870f153c873a4f117b758
Joseph Riandy Pratama Hutagalung <95567038+bulee021@users.noreply.github.com>
2024-03-06 16:16:56 +0700

    New Project
    Create new repository for project E-Commerce XSIS

---
+++ e-commerce/src/test/java/com/xsis/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsis.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
